10c0ec4ee0341bcbde0520c7e23102063ace0310
christianghv <christianghv@gmail.com>
2022-04-18 16:11:17 -0400

    Add files via upload

---
+++ src/test/java/testscenario/ScenarioJunitActivatorTest.java
@@ -0,0 +1,10 @@
+package testscenario;
+/**
+* Do not remove this file
+*/
+@org.junit.runner.RunWith(org.drools.scenariosimulation.backend.runner.ScenarioJunitActivator.class)
+/**
+   * Escenario de Pruebas. 
+   */
+public class ScenarioJunitActivatorTest {
+}
